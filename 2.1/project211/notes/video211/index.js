195ff7de278b003b194d7b12057f749c16079b33
RFNoC <rongjun-geng@rfnocs-mac-pro.lan>
2021-09-27 06:48:50 -0700

    add lecture 211 video codes

---
+++ 2.1/project211/notes/video211/index.js
@@ -0,0 +1,6 @@
+// Follow the instructions provided in training kit and then follow along here.
+
+const secondHeading = document.querySelector('.second-heading');
+console.log(secondHeading.textContent);
+secondHeading.textContent = "Updated Content";
+console.log(secondHeading.textContent);
